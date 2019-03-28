57716e348ac8eea3e0755c6c78baaaf992de25d3
Zachary Owen <zach.owen50@gmail.com>
2019-03-27 21:45:44 -0700

    Whole bunch of stuff.

--- index.js
+++ index.js
@@ -1,98 +1,148 @@
-document.body.addEventListener("keyup", function(e) {
+$(document).keyup(function (e) {
   switch (e.key) {
-    case "w":
-      moveUp("knight");
+    case 'w':
+      moveUp('knight');
       break;
-
-    case "a":
-      moveLeft("knight");
+    case 'a':
+      moveLeft('knight');
       break;
-
-    case "s":
-      moveDown("knight");
-    break;
-
-    case "d":
-      moveRight("knight");
+    case 's':
+      moveDown('knight');
       break;
-
-    case "i":
-      moveUp("dino");
+    case 'd':
+      moveRight('knight');
       break;
-
-    case "j":
-      moveLeft("dino");
+    case 'i':
+      moveUp('dino');
       break;
-
-    case "k":
-      moveDown("dino");
+    case 'j':
+      moveLeft('dino');
       break;
-
-    case "l":
-      moveRight("dino");
+    case 'k':
+      moveDown('dino');
       break;
-
+    case 'l':
+      moveRight('dino');
+      break;
     default:
       break;
   }
 });
 
-document.getElementsByClassName("knight-move-right")[0].addEventListener('click',function(e){
-  moveRight("knight");
+$('.knight-move-right').click(function () {
+  moveRight('knight');
 });
 
-document.getElementsByClassName("knight-move-left")[0].addEventListener('click',function(e){
-  moveLeft("knight");
+$('.knight-move-left').click(function () {
+  moveLeft('knight');
 });
 
-document.getElementsByClassName("knight-move-up")[0].addEventListener('click',function(e){
-  moveRight("knight");
+$('.knight-move-up').click(function () {
+  moveUp('knight');
 });
 
-document.getElementsByClassName("knight-move-down")[0].addEventListener('click',function(e){
-  moveDown("knight");
+$('.knight-move-down').click(function() {
+  moveDown('knight');
 });
 
-document.getElementsByClassName("dino-move-right")[0].addEventListener('click',function(e){
-  moveRight("dino");
+$('.dino-move-right').click(function () {
+  moveRight('dino');
 });
-document.getElementsByClassName("dino-move-left")[0].addEventListener('click',function(e){
-  moveLeft("dino");
+
+$('.dino-move-left').click(function () {
+  moveLeft('dino');
 });
-document.getElementsByClassName("dino-move-up")[0].addEventListener('click',function(e){
-  moveUp("dino");
+
+$('.dino-move-up').click(function () {
+  moveUp('dino');
 });
-document.getElementsByClassName("dino-move-down")[0].addEventListener('click',function(e){
-  moveDown("dino");
+
+$('.dino-move-down').click(function () {
+  moveDown('dino');
 });
 
 function moveUp(player) {
-  var currentIndex = parseInt(document.getElementsByClassName(player)[0].classList[0]);
+  
+  var currentIndex = parseInt($('.' + player)[0].classList[2]);
   if (currentIndex > 8) {
-    document.getElementsByClassName(player)[0].classList.remove(player);
-    document.getElementsByClassName(currentIndex - 8)[0].classList.add(player);
+
+    // $('.dino').fadeOut( 150, function() {
+    //   // Animation complete.
+    //   $('.dino').fadeIn(150, function() {
+    //       $('.' + player).first().removeClass(player);
+    //       $('.grid-container').children('.' + (currentIndex - 8)).addClass(player);
+    //   });
+    // });
+
+    $('.' + player).first().removeClass(player);
+    $('.grid-container').children('.' + (currentIndex - 8)).addClass(player);
   }
 }
 function moveDown(player) {
-  var currentIndex = parseInt(document.getElementsByClassName(player)[0].classList[0]);
-    if (currentIndex < 57) {
-      document.getElementsByClassName(player)[0].classList.remove(player);
-      document.getElementsByClassName(currentIndex + 8)[0].classList.add(player);
+  var currentIndex = parseInt($('.' + player)[0].classList[2]);
+     if (currentIndex < 57) {
+      // $('.dino').fadeOut( 150, function() {
+      //   // Animation complete.
+      //   $('.dino').fadeIn(150, function() {
+      //     $('.' + player).first().removeClass(player);
+      //     $('.grid-container').children('.' + (currentIndex + 8)).addClass(player);
+      //   });
+      // });
+      $('.' + player).first().removeClass(player);
+      $('.grid-container').children('.' + (currentIndex + 8)).addClass(player);
     }
 }
 function moveLeft(player) {
-  var currentIndex = parseInt(document.getElementsByClassName(player)[0].classList[0]);
+  var currentIndex = parseInt($('.' + player)[0].classList[2]);
   currentIndex -= 1;
   if (currentIndex > 0){
-    document.getElementsByClassName(player)[0].classList.remove(player);
-    document.getElementsByClassName(currentIndex)[0].classList.add(player);
+    // $('.dino').fadeOut( 150, function() {
+    //   // Animation complete.
+    //   $('.dino').fadeIn(150, function() {
+    //     $('.' + player).first().removeClass(player);
+    //     $('.grid-container').children('.' + currentIndex).addClass(player);
+    //   });
+    // });
+
+    $('.' + player).first().removeClass(player);
+    $('.grid-container').children('.' + currentIndex).addClass(player);
   }
 }
 function moveRight(player) {
-  var currentIndex = parseInt(document.getElementsByClassName(player)[0].classList[0]);
+  var currentIndex = parseInt($('.' + player)[0].classList[2]);
   currentIndex += 1;
   if (currentIndex <= 64) {
-    document.getElementsByClassName(player)[0].classList.remove(player);
-    document.getElementsByClassName(currentIndex)[0].classList.add(player);
+
+    // $('.dino').fadeOut( 150, function() {
+    //   // Animation complete.
+    //   $('.dino').fadeIn(150, function() {
+    //     $('.' + player).first().removeClass(player);
+    //     $('.grid-container').children('.' + currentIndex).addClass(player);
+    //   });
+    // });
+
+    $('.' + player).first().removeClass(player);
+    $('.grid-container').children('.' + currentIndex).addClass(player);
   }
 }
+
+// Dynamically Create Grid or Table for Playing Field:
+$(function() {
+  console.log( 'Tested index.js!' );
+  let colCount = 1;
+  
+  for (let column = 1; column <= 8; column++) {
+    for (let row = 1; row <= 8; row++) {
+      var tile = $('<div>').addClass('grid-tile row-' + column + ' ' + colCount);
+      colCount += 1;
+      $('.grid-container').append(tile);
+    }
+  }
+
+  // Knight:
+  $('.1').addClass('knight');
+  
+  // Dino:
+  $('.2').addClass('dino');
+
+});
